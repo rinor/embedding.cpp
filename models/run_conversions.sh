8ca9a1abf3c379bdd604167f70131e4823b26915
FFengIll <fengyouzheng@gmail.com>
2023-09-26 14:28:32 +0800

    feat: set gguf as default.

--- models/run_conversions.sh
+++ models/run_conversions.sh
@@ -2,7 +2,7 @@
 
 model=$1
 
-python3 convert-to-ggml.py ${model}/ 0
-python3 convert-to-ggml.py ${model}/ 1
-../build/bin/quantize ${model}/ggml-model-f16.bin ${model}/ggml-model-q4_0.bin 2
-../build/bin/quantize ${model}/ggml-model-f16.bin ${model}/ggml-model-q4_1.bin 3
+python3 convert-to-gguf.py ${model}/ 0
+python3 convert-to-gguf.py ${model}/ 1
+../build/bin/quantize ${model}/ggml-model-f16.gguf ${model}/ggml-model-q4_0.gguf 2
+../build/bin/quantize ${model}/ggml-model-f16.gguf ${model}/ggml-model-q4_1.gguf 3
